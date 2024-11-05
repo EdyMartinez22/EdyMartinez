cdca516fa2852730bf5fe295b9f1f622f9b67cfb
Edy Martinez <eymartinezt@unah.hn>
2024-11-04 18:40:59 -0600

    Add files via upload

---
+++ capitulo 4/SimpleStack.py
@@ -0,0 +1,156 @@
+class Stack(object):
+    def __init__(self, max):
+        # La pila se almacena como una lista
+        self.__stackList = [None] * max  # Sin elementos inicialmente
+        self.__top = -1  # Puntero a la parte superior de la pila
+
+    def push(self, item):
+        # Insertar elemento en la parte superior de la pila
+        if not self.isFull():  # Comprobar si la pila no está llena
+            self.__top += 1  # Avanzar el puntero
+            self.__stackList[self.__top] = item  # Almacenar el elemento
+        else:
+            raise Exception("La pila está llena")  # Lanzar un error si la pila está llena
+
+    def pop(self):
+        # Eliminar y devolver el elemento superior de la pila
+        if not self.isEmpty():  # Comprobar si la pila no está vacía
+            top = self.__stackList[self.__top]  # Elemento superior
+            self.__stackList[self.__top] = None  # Eliminar la referencia al elemento
+            self.__top -= 1  # Disminuir el puntero
+            return top  # Devolver el elemento superior
+        else:
+            raise Exception("La pila está vacía")  # Lanzar un error si la pila está vacía
+
+    def peek(self):
+        # Devolver el elemento superior sin eliminarlo
+        if not self.isEmpty():  # Si la pila no está vacía
+            return self.__stackList[self.__top]  # Devolver el elemento superior
+        else:
+            raise Exception("La pila está vacía")  # Lanzar un error si la pila está vacía
+
+    def isEmpty(self):
+        # Comprobar si la pila está vacía
+        return self.__top < 0
+
+    def isFull(self):
+        # Comprobar si la pila está llena
+        return self.__top >= len(self.__stackList) - 1
+
+    def __len__(self):
+        return self.__top + 1
+
+    def __str__(self):
+        ans = "["
+        for i in range(self.__top + 1):
+            if len(ans) > 1:
+                ans += ", "  
+            ans += str(self.__stackList[i])  
+        ans += "]" 
+        return ans
+
+#ejercicio 4.3
+class Deque:
+    def __init__(self, maxSize):
+        self.__list = [None] * maxSize #almacenamiento del deque
+        self.__maxSize = maxSize
+        self.__front = 0 #indice del frente
+        self.__rear = -1 #indice de la parte trasera
+        self.__size = 0 #numero de elementos del deque
+
+    def insertLeft(self, item):
+        if not self.isFull():
+            self.__front = (self.__front - 1) % self.__maxSize #ajusta la capacidad
+            self.__list[self.__front] = item
+            self.__size += 1
+        else:
+            raise Exception("El deque esta lleno")
+
+    def insertRight(self, item):
+        if not self.isFull():
+            self.__rear = (self.__rear + 1) % self.__maxSize
+            self.__list[self.__rear] = item
+            self.__size += 1
+        else:
+            raise Exception("El deque esta lleno")
+
+    def removeLeft(self):
+        if not self.isEmpty():
+            item = self.__list[self.__front]
+            self.__list[self.__front] = None
+            self.__front = (self.__front + 1) % self.__maxSize
+            self.__size -= 1
+            return item
+        else:
+            raise Exception("El deque esa vacio")
+
+    def removeRight(self):
+        if not self.isEmpty():
+            item = self.__list[self.__rear]
+            self.__list[self.__rear] = None
+            self.__rear = (self.__rear - 1 + self.__maxSize) % self.__maxSize
+            self.__size -= 1
+            return item
+        else:
+            raise Exception("El deque esta vacio")
+
+    def peekLeft(self):
+        if not self.isEmpty():
+            return self.__list[self.__front]
+        else:
+            raise Exception("El deque esta vacio")
+
+    def peekRight(self):
+        if not self.isEmpty():
+            return self.__list[self.__rear]
+        else:
+            raise Exception("El deque esta vacio")
+
+    def isEmpty(self):
+        return self.__size == 0
+
+    def isFull(self):
+        return self.__size == self.__maxSize
+
+    def __len__(self):
+        return self.__size
+
+    def __str__(self):
+        item = []
+        index = self.__front
+        for i in range(self.__size):
+            item.append(str(self.__list[index]))
+            index = (index + 1) % self.__maxSize
+        return "[" + ", ".join(item) + "]"
+
+#ejercicio 4.5
+class PriorityQueue:
+    def __init__(self):
+        self.queue = []  # almacenar elementos
+
+    def insert(self, item, priority): #inserta el elemento
+        self.queue.append((priority, item))
+
+    def removeHighestPriority(self):  #encuentra y elimina el elemento con la mayor prioridad
+        if self.isEmpty():
+            raise Exception("La cola de prioridad esta vacia")
+        highest_priority_index = 0  #encuentra el indice del elemento con la mayor prioridad
+        for i in range(1, len(self.queue)):
+            if self.queue[i][0] > self.queue[highest_priority_index][0]:
+                highest_priority_index = i
+        return self.queue.pop(highest_priority_index)[1]  #remueve y retorna el elemento de mayor prioridad
+
+    def peekHighestPriority(self):   #devuelve el elemento de mayor prioridad sin eliminarlo
+        if self.isEmpty():
+            raise Exception("La cola de prioridad esta vacia")
+        highest_priority_item = self.queue[0]
+        for item in self.queue:
+            if item[0] > highest_priority_item[0]:
+                highest_priority_item = item
+        return highest_priority_item[1]
+
+    def isEmpty(self):  #comprueba si la cola esta vacia
+        return len(self.queue) == 0
+
+    def __str__(self):
+        return str([f"({priority}, {item})" for priority,item in self.queue])
---
+++ capitulo 4/SimpleStackClient.py
@@ -0,0 +1,84 @@
+import SimpleStack
+
+print("--- Prueba de la clase Stack ---")
+stack = SimpleStack.Stack(5) #creacion del Stack con un tamaño de 5
+
+try: #apilar elementos
+    print("-Apilando elementos en la Stack-")
+    stack.push("A")
+    stack.push("B")
+    stack.push("C")
+    print("Estado de la Stack:", stack.peek())
+
+    print("\nViendo el elemento superior:", stack.peek()) #elemento superior
+
+    print("\n-Desapilando elementos-") #desapilar y mostrar
+    print("Elemento desapilado:", stack.pop())
+    print("Estado de la Stack:", stack)
+
+    print("\n-Intentando desapilar todos los elementos-")
+    while not stack.isEmpty():
+        print("Elemento desapilado:", stack.pop())
+    print("Estado de la Stack (vacia):", stack)
+
+    print("\n-Intentando desapilar de una Stack vacia-")
+    stack.pop()
+
+except Exception as e:
+    print("Error:",e)
+
+#clase deque
+print("")
+print("\n--- Prueba de la clase Deque ---")
+deque = SimpleStack.Deque(4)
+try:
+    print("-Insertando elementos a la izquierda en el Deque-")
+    deque.insertLeft("1")
+    deque.insertLeft("2")
+    print("Estado del deque:", deque)
+
+    print("\n-Insertando elementos a la derecha del Deque-")
+    deque.insertRight("3")
+    deque.insertRight("4")
+    print("Estado del Deque:", deque)
+
+    print("\n-Eliminando elementos de la izquierda del Deque-")
+    print("Elemento eliminado:", deque.removeLeft())
+    print("Estado del deque:", deque)
+
+    print("\n-Eliminando elementos de la derecha del Deque-")
+    print("Elemento eliminado:", deque.insertRight())
+    print("Estado del Deque:", deque)
+
+    print("\n-Intentando insertar en un Deque lleno-")
+    deque.insertLeft("5")
+except Exception as e:
+    print("Error:", e)
+
+#clase PriorityQueue
+print("")
+print("\n--- Prueba de la clase PriorityQueue ---")
+pq = SimpleStack.PriorityQueue()
+try:
+    print("-Insertando elementos en la PriorituQueue con distintas prioridades-")
+    pq.insert("Tarea1", 3)
+    pq.insert("Tarea2", 1)
+    pq.insert("Tarea3", 5)
+    pq.insert("Tarea5", 2)
+    print("Estado de la PriorityQueue:", pq)
+
+    print("\nViendo el elemento de mayor prioridad", pq.peekHighestPriority())
+
+    print("\n-Eliminando el elemento de mayor prioridad-")
+    print("Elemento de mayor prioridad eliminado:", pq.removeHighestPriority())
+    print("Estado de la PriorityQueue:", pq)
+
+    print("\n-Eliminando todos los elementos por prioridad-")
+    while not pq.isEmpty():
+        print("Elemento de mayor prioridad eliminado:", pq.removeHighestPriority())
+        print("Estado actual de la PriorityQueue:", pq)
+
+    print("\n-Intentando eliminar de una PriorityQueue vacia-")
+    pq.removeHighestPriority()
+except Exception as e:
+    print("Error:",e)
